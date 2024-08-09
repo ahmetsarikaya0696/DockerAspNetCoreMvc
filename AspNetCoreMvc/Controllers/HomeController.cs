f15ace54cc847a2667f2fdd779098d5c0e3a08d9
ahmetsarikaya0696 <ahmetsarikaya0696@gmail.com>
2024-08-09 11:48:49 +0300

    bind-mount ve volume

--- AspNetCoreMvc/Controllers/HomeController.cs
+++ AspNetCoreMvc/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using AspNetCoreMvc.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
 using System.Diagnostics;
 
 namespace AspNetCoreMvc.Controllers
@@ -7,10 +8,12 @@ namespace AspNetCoreMvc.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IFileProvider _fileProvider;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IFileProvider fileProvider)
         {
             _logger = logger;
+            _fileProvider = fileProvider;
         }
 
         public IActionResult Index()
@@ -27,6 +30,45 @@ namespace AspNetCoreMvc.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult ImageSave()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ImageSave(IFormFile imageFile)
+        {
+            if (imageFile != null && imageFile.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/", "images", fileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await imageFile.CopyToAsync(stream);
+                }
+
+            }
+
+            return View();
+        }
+
+        public IActionResult ImageShow()
+        {
+            var images = _fileProvider.GetDirectoryContents("wwwroot/images").Select(x => x.Name).ToList();
+
+            return View(images);
+        }
+
+        [HttpPost]
+        public IActionResult ImageShow(string name)
+        {
+            var file = _fileProvider.GetDirectoryContents("wwwroot/images").First(x => x.Name == name);
+            System.IO.File.Delete(file.PhysicalPath);
+                
+            return RedirectToAction(nameof(ImageShow));
         }
     }
 }
--- AspNetCoreMvc/Program.cs
+++ AspNetCoreMvc/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+IFileProvider fileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
+builder.Services.AddSingleton(fileProvider);
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
